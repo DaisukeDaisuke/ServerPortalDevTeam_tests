e7f0782ab4d6704dceaf58b3e7d79800b675a418
DaisukeDaisuke <akblcmakblcm@gmail.com>
2018-01-05 20:25:01 +0900

    test

--- Portal/Portal.php
+++ Portal/Portal.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace unphar;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+
+class unphar extends unpharcommand implements Listener{
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+}
